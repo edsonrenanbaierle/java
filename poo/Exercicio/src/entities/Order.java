61e319a39ebb4864ded426bb016b46a1ab73fd41
Edson Baierle <edsonbaierle@gmail.com>
2024-05-06 22:43:14 -0300

    Exercicio POO parte 1, definidas as classes, enums

---
+++ poo/Exercicio/src/entities/Order.java
@@ -0,0 +1,60 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entities;
+
+import enums.OrderStatus;
+import java.util.ArrayList;
+import java.util.Date;
+
+/**
+ *
+ * @author User
+ */
+public class Order {
+    private Date moment;
+    private OrderStatus status;
+    private Cliente cliente;
+    private ArrayList<OrderItem> orderItem= new ArrayList<OrderItem>();
+
+    public Order(Date moment, OrderStatus status, Cliente cliente) {
+        this.moment = moment;
+        this.status = status;
+        this.cliente = cliente;
+    }
+
+    public Date getMoment() {
+        return moment;
+    }
+
+    public void setMoment(Date moment) {
+        this.moment = moment;
+    }
+
+    public OrderStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(OrderStatus status) {
+        this.status = status;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public ArrayList<OrderItem> getOrderItem() {
+        return orderItem;
+    }
+
+    public void setOrderItem(ArrayList<OrderItem> orderItem) {
+        this.orderItem = orderItem;
+    }
+    
+    
+}
